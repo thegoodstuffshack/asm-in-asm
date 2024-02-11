22d7105313a36a23806978b47ee91474593ad917
thegoodstuffshack <thegoodstuffshack@gmail.com>
2024-02-11 16:28:40 +1100

    experiment with video mode 3

--- boot.asm
+++ boot.asm
@@ -3,7 +3,27 @@
 jmp start
 
 start:
+	; set video mode 3
+	mov ah, 0
+	mov al, 0x03
+	int 0x10
+
+	mov al, 14
+	call printChar
+
+	push 0xB800
+	pop es
+	push word [es:0x0002]
+	push word [es:0x0000]
+	call printHex
+	call printHex
+
+	mov word [es:0x00F0], 0x0780
+	mov word [es:0x00F2], 0x0F0E
+	
 	jmp $
+
+%include "print.asm"
 
 times 510-($-$$) db 0
 dw 0xAA55
---
+++ print.asm
@@ -0,0 +1,45 @@
+;; print.asm
+
+; al as input
+printChar:
+	mov ah, 0x0e
+	int 0x10
+	ret
+
+;
+; printString:
+	; mov
+
+; push ax as input
+printHex:
+	push bp
+	mov bp, sp
+
+	mov cx, 4
+.loop:
+	push cx
+	mov ax, 4
+	mov bx, 3
+	dec cx
+	sub bx, cx
+	mul bx
+	mov cx, ax
+	mov ax, word [bp+4]
+	shl ax, cl
+	shr ax, 12
+	
+	cmp al, 10
+	jb .number
+	mov ah, 55
+	jmp .character
+.number:
+	mov ah, 48
+.character:
+	add al, ah
+	mov ah, 0x0e,
+	int 0x10
+	pop cx
+	loop .loop
+.end:
+	pop bp
+	ret 2	; pop 2 bytes off stack
